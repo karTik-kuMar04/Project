edd50f95e9fb125bddb6d49fc4625d2d3eb9336b
kartik <mysoulisinfinity1@gmail.com>
2026-06-08 22:47:01 +0530

    Initial setup for project in next

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { motion } from 'motion/react';
+
+
+export default function Home() {
+    return (
+        <motion.div>
+            Home Page
+        </motion.div>
+    )
+}
